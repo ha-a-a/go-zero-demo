f6ffd5508419d37ba81d03e3b488a986522c9a18
mengyuetang <mengyuetang@sohu-inc.com>
2022-09-02 16:04:40 +0800

    jaeger 链路追踪

--- mall/user/rpc/internal/config/config.go
+++ mall/user/rpc/internal/config/config.go
@@ -1,7 +1,11 @@
 package config
 
-import "github.com/zeromicro/go-zero/zrpc"
+import (
+	"github.com/zeromicro/go-zero/core/trace"
+	"github.com/zeromicro/go-zero/zrpc"
+)
 
 type Config struct {
 	zrpc.RpcServerConf
+	Telemetry trace.Config
 }
